b2bd9b11c9a0094cb901e1dfe43fd7d4464d9f41
pathymavuba <mavubapathy@gmail.com>
2022-08-03 17:59:33 +0100

    startignPage done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './index.css'
+import Card from './component/Card'
+import StartingPage from './component/StartingPage'
 
 const App = () => {
   return (
-    <body></body>
+    <div>
+      <StartingPage />
+    </div>
   )
 }
 
---
+++ src/component/StartingPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './StartingPage.css'
+import Card from './Card'
+
+const StartingPage = () => {
+  return (
+    <Card>
+    <h1 className='header'>Welcome To Quiz Game</h1>
+    <h3 className='primary_text'>please enter your username</h3>
+    <input type="text" className="user_name"  placeholder='username'/>
+    <button className="start_btn">let's paly</button>
+    <p className="top_score">
+        Top score : <span>0</span>
+    </p>
+
+
+</Card>
+  )
+}
+
+export default StartingPage
